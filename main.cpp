cdd58ad5e84be3c2011df7ea06486dc505346ea7
LuckyLizard <118721204+LuckyLizard-MKEL1123@users.noreply.github.com>
2023-02-26 22:06:15 +0800

    Update and rename main.c to main.cpp

--- main.cpp
+++ main.cpp
@@ -18,18 +18,16 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "stdio.h"
-#include <string.h>
-//#include "kws_ds_cnn.h"
+#include "edge-impulse-sdk/classifier/ei_run_classifier.h"
+#define BUFFER_SIZE 16
+
+uint8_t buf[12];
+int16_t data_in[2];
+int16_t audio_buffer[16000];
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-uint8_t buf[12];
-uint16_t data_in[2];
-int16_t audio_buffer[16000];
-int keep_command;
-int i=0;
-int j=0;
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -48,7 +46,8 @@ int j=0;
 
 /* Private variables ---------------------------------------------------------*/
 I2S_HandleTypeDef hi2s2;
-DMA_HandleTypeDef hdma_spi2_tx;
+
+USART_HandleTypeDef husart2;
 
 /* USER CODE BEGIN PV */
 
@@ -57,20 +56,37 @@ DMA_HandleTypeDef hdma_spi2_tx;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_DMA_Init(void);
 static void MX_I2S2_Init(void);
+static void MX_USART2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-int HELP_command() {
-	//Red LED turn on
-	HAL_Write_Pin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+//'help' output command
+void HELP_command() {
+	//whenever sound detected, the green LED turn on
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	HAL_Delay(2000);
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	HAL_Delay(100);
+
 	// Green LED will show MORSE code 'SOS'
-	int code;
-	int mcode[18]={1,1,1,1,1,1,2,1,2,1,2,1,1,1,1,1,1,1};
+	int i;
+	int a[18]={1,1,1,1,1,1,2,1,2,1,2,1,1,1,1,1,1,1};
 	for (i=0;i<18;i++){
 		  		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
 
@@ -81,26 +97,55 @@ int HELP_command() {
 		  			HAL_Delay(900);
 		  		}
 	}
-	//Buzzer turn off
-	HAL_Write_Pin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
 }
 
-int KILL_command() {
-	//Red LED blink
-	HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_1);
+//output when detect the 'kill' keyword
+void KILL_command() {
+	//whenever sound detected, the green LED turn on
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	HAL_Delay(2000);
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	HAL_Delay(100);
+
+    //Red LED blink
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
 	HAL_Delay(500);
-	//Green LED turn OFF
-	HAL_Write_Pin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-	//Buzzer will be activated
-	HAL_Write_Pin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_Delay(500);
+
 }
 
-/* USER CODE END 0 */
+//if the system detect other sound other than 'help' and 'kill'
+void OTHER_command() {
+	//whenever sound detected, the green LED turn on
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
+	HAL_Delay(2000);
+}
+void copy_buffer_to_classifier_input_buffer(const int8_t* buffer) {
+    int buffer_offset = 0;
+    for (size_t ix = 0; ix < CLASSIFIER_INPUT_BUFFER_SIZE; ix++) {
+        float s = (float) buffer[buffer_offset];
+        classifier_input_buffer[ix] = s / 127.0f;
+        buffer_offset += 2; // assuming int16_t data type
+    }
+}
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -113,7 +158,7 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
+ 
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -125,8 +170,8 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
   MX_I2S2_Init();
+  MX_USART2_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -135,47 +180,41 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_Delay(100);
     /* USER CODE END WHILE */
-	  for (int k=0; k<16000; k+=2){
-	  		  volatile HAL_StatusTypeDef result = HAL_I2S_Receive(&hi2s2, data_in, 2, 100);
-	  		  if (result != HAL_OK){
-	  			  strcpy((char*)buf, "Error Rx\r\n");
-	  		  } else {
-	  			  audio_buffer[k] = (int16_t)data_in[0];
-	  			  audio_buffer[k+1] = (int16_t)data_in[1];
-	  		  }
-	  	  }
-
-	 	  KWS_DS_CNN kws(audio_buffer);
-
-	 	  kws.extract_features();
-	 	  kws.classify();
-
-	 	  int max_ind = kws.get_top_class(kws.output);
-
-	 	  char buffer [70];
-	 	  int buffer_output = 0;
-
-	 	  buffer_output = sprintf(buffer, "Detected %s (%d%%)\r\n", output_class[max_ind], ((int)kws.output[max_ind]*100/128));
-
-	 	 //char output_class[12][8] = {"Silence", "Unknown", "yes", "no", "up", "down", "left", "right", "on", "off", "stop", "go"};
-	 	 //Double check this part
-	 	HAL_Write_Pin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
-	 	HAL_Write_Pin(GPIOB, GPIO_PIN_1, GPIO_PIN_RESET);
-	 	HAL_Write_Pin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
-
-	 	 char output_class[4][8] = {"Silence", "Unknown", "help", "kill"};
-	 	 if (max_ind == 2 ){	//help
-	 	 			  HELP_command();
-	 	 			  //keep_command = 1; just so that the wheel keep turning as previous
-	 	 		  }
-	 	 		  else if (max_ind == 3){	//kill
-	 	 			  KILL_command();
-	 	 			  //keep_command = 0;
-	 	 		  }
+	  for (int i=0; i< 16000; i+2) {
+	   	    volatile HAL_StatusTypeDef result = HAL_I2S_Receive(&hi2s2, data_in, 2, 100);
+	   	  	  if (result != HAL_OK){
+	   	  			  strcpy((char*) buf, "Error Rx");
+	   	  		  }
+	   	  		  else {
+	   	  			  audio_buffer[1] = data_in[0];
+	   	  			  audio_buffer[i+1] = data_in[1];
+	   	  		  }
+	   	  	  }
+	   	//  signal_t signal;
+	   	//  signal.total_length = sizeof(features) / sizeof(features[0]);
+	   //	  signal.get_data = &get_audio_signal_data;
 
 
+	       ei_impulse_result_t result = { 0 };
+	       EI_IMPULSE_ERROR res = run_classifier(&signal, &result, true);
+	        for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++){
+	     	   if (result.classification[0].value > 0.5) {
+	     	       if (strcmp(result.classification[0].label, "kill") == 0) {
+	     	           // the input buffer data contains "kill"
+	     	    	   KILL_command();
+	     	       } else if (strcmp(result.classification[0].label, "help") == 0) {
+	     	           // the input buffer data contains "help"
+	     	    	   HELP_command();
+	     	       } else {
+	     	           // unexpected result
+	     	    	   OTHER_command();
+	     	       }
+	     	   } else {
+	     	       // no prediction
+	     	   }
+	     	   
+	        }
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -185,18 +224,11 @@ int main(void)
   * @brief System Clock Configuration
   * @retval None
   */
+
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Macro to configure the PLL multiplication factor
-  */
-  __HAL_RCC_PLL_PLLM_CONFIG(16);
-
-  /** Macro to configure the PLL clock source
-  */
-  __HAL_RCC_PLL_PLLSOURCE_CONFIG(RCC_PLLSOURCE_HSI);
 
   /** Configure the main internal regulator output voltage
   */
@@ -209,8 +241,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -220,12 +256,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
@@ -249,9 +285,9 @@ static void MX_I2S2_Init(void)
   hi2s2.Instance = SPI2;
   hi2s2.Init.Mode = I2S_MODE_MASTER_TX;
   hi2s2.Init.Standard = I2S_STANDARD_PHILIPS;
-  hi2s2.Init.DataFormat = I2S_DATAFORMAT_24B;
+  hi2s2.Init.DataFormat = I2S_DATAFORMAT_16B;
   hi2s2.Init.MCLKOutput = I2S_MCLKOUTPUT_DISABLE;
-  hi2s2.Init.AudioFreq = I2S_AUDIOFREQ_32K;
+  hi2s2.Init.AudioFreq = I2S_AUDIOFREQ_8K;
   hi2s2.Init.CPOL = I2S_CPOL_LOW;
   hi2s2.Init.ClockSource = I2S_CLOCK_PLL;
   hi2s2.Init.FullDuplexMode = I2S_FULLDUPLEXMODE_DISABLE;
@@ -266,18 +302,36 @@ static void MX_I2S2_Init(void)
 }
 
 /**
-  * Enable DMA controller clock
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
   */
-static void MX_DMA_Init(void)
+static void MX_USART2_Init(void)
 {
 
-  /* DMA controller clock enable */
-  __HAL_RCC_DMA1_CLK_ENABLE();
+  /* USER CODE BEGIN USART2_Init 0 */
 
-  /* DMA interrupt init */
-  /* DMA1_Stream4_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Stream4_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA1_Stream4_IRQn);
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  husart2.Instance = USART2;
+  husart2.Init.BaudRate = 115200;
+  husart2.Init.WordLength = USART_WORDLENGTH_8B;
+  husart2.Init.StopBits = USART_STOPBITS_1;
+  husart2.Init.Parity = USART_PARITY_NONE;
+  husart2.Init.Mode = USART_MODE_TX_RX;
+  husart2.Init.CLKPolarity = USART_POLARITY_LOW;
+  husart2.Init.CLKPhase = USART_PHASE_1EDGE;
+  husart2.Init.CLKLastBit = USART_LASTBIT_DISABLE;
+  if (HAL_USART_Init(&husart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
 
 }
 
@@ -292,16 +346,21 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(Push_Button_GPIO_Port, Push_Button_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, Green_LED_Pin|Red_LED_Pin|Buzzer_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  /*Configure GPIO pin : Push_Button_Pin */
+  GPIO_InitStruct.Pin = Push_Button_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(Push_Button_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : Green_LED_Pin Red_LED_Pin Buzzer_Pin */
   GPIO_InitStruct.Pin = Green_LED_Pin|Red_LED_Pin|Buzzer_Pin;
